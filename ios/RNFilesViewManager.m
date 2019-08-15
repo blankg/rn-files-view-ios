7a2200133c19c3f60a3e5d47a34ffb0dc038f623
Guy Blank <blank.guy@gmail.com>
2019-08-15 13:41:26 +0300

    add onPreviewFailed event prop

--- ios/RNFilesViewManager.m
+++ ios/RNFilesViewManager.m
@@ -15,6 +15,6 @@ RCT_EXPORT_VIEW_PROPERTY(urls, NSArray)
 RCT_EXPORT_VIEW_PROPERTY(onFileChange, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(previewBackgroundColor, UIColor)
 RCT_EXPORT_VIEW_PROPERTY(initialIndex, NSInteger)
-
+RCT_EXPORT_VIEW_PROPERTY(onPreviewFailed, RCTBubblingEventBlock)
 
 @end
